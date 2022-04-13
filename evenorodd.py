10b065f8d0be05c7df3413873d24a9121aa56763
Amirsam Goharpey <50692220+amirsamgoharpay@users.noreply.github.com>
2022-04-13 20:00:51 +0430

    Update evenorodd.py

--- evenorodd.py
+++ evenorodd.py
@@ -1,19 +1,4 @@
-'''x = input("adad ra vared konid : ")
-def evenorodd(y):
-    y = list(str(y))
-    y  = y[-1]
-    y = int(y)
-    if y%2 == 0 :
-        print("even")
-    elif y%2 == 1 :
-        print("odd")
 
-try :
-    x = int(x)
-    evenorodd(x)
-except ValueError as error :
-    print("Invalid")
-'''
 def eod(x):
     try :
         x = int(x)
